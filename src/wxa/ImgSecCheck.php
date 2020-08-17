bffde6d9f36e377eb4a325fbf7cf435462a1031c
yls <yelinsen@mayitanlu.cn>
2020-08-17 20:18:23 +0800

    违规验证接口

---
+++ src/wxa/ImgSecCheck.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types = 1);
+
+namespace woodlsy\wechatRest\wxa;
+
+use woodlsy\wechatRest\Request;
+
+class ImgSecCheck extends Request
+{
+    public function __construct()
+    {
+        $this->queryUrl = '/wxa/msg_sec_check';
+    }
+
+    /**
+     * 设置第三方access token
+     *
+     * @author yls
+     * @param string $accessToken
+     * @return $this
+     */
+    public function setAccessToken(string $accessToken) : ImgSecCheck
+    {
+        $this->queryParams['access_token'] = $accessToken;
+        $this->params['access_token'] = $accessToken;
+        return $this;
+    }
+
+    /**
+     * 设置图片
+     *
+     * @author yls
+     * @param string $media
+     * @return $this
+     */
+    public function setMedia(string $media) : ImgSecCheck
+    {
+        $this->params['media'] = $media;
+        return $this;
+    }
+
+    /**
+     * 获取参数
+     *
+     * @author yls
+     * @return $this
+     */
+    public function getParams() : ImgSecCheck
+    {
+        $this->params = "{}";
+        return $this;
+    }
+
+}
---
+++ src/wxa/MsgSecCheck.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types = 1);
+
+namespace woodlsy\wechatRest\wxa;
+
+use woodlsy\wechatRest\Request;
+
+class MsgSecCheck extends Request
+{
+    public function __construct()
+    {
+        $this->queryUrl = '/wxa/msg_sec_check';
+    }
+
+    /**
+     * 设置第三方access token
+     *
+     * @author yls
+     * @param string $accessToken
+     * @return $this
+     */
+    public function setAccessToken(string $accessToken) : MsgSecCheck
+    {
+        $this->queryParams['access_token'] = $accessToken;
+        $this->params['access_token'] = $accessToken;
+        return $this;
+    }
+
+    /**
+     * 设置内容
+     *
+     * @author yls
+     * @param string $content
+     * @return $this
+     */
+    public function setContent(string $content) : MsgSecCheck
+    {
+        $this->params['content'] = $content;
+        return $this;
+    }
+
+    /**
+     * 获取参数
+     *
+     * @author yls
+     * @return $this
+     */
+    public function getParams() : MsgSecCheck
+    {
+        $this->params = "{}";
+        return $this;
+    }
+
+}
